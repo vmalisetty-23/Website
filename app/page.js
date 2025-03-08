a20831e1f992d83d6cd2d8c57ca2fefe56eba465
Vasishta Malisetty <malisetty.v@northeastern.edu>
2025-03-08 12:52:56 -0500

    Completed links on Home Page

--- app/page.js
+++ app/page.js
@@ -2,20 +2,20 @@ export default function Home() {
   return (
     <>
     <div>
-      <h1 className="container mx-auto flex justify-center text-5xl">Vasishta Malisetty</h1>
-      <p className="container mx-auto flex justify-center p-4 text-2xl">Electrical & Computer Engineering @ Northeastern</p>
-      <img className="rounded-full w-64 h-64 container mx-auto p-4" src="/assets/images/Website-Photo.png"></img>
-      <a href="/assets/files/Vasishta-Malisetty-Resume-2025.pdf" target="_blank" rel="noopener noreferrer" className="flex w-full max-w-24 container mx-auto outline-solid bg-black-500 hover:bg-white text-white hover:text-black font-bold py-2 px-4 rounded">
+      <h1 className="container mx-auto flex justify-center text-5xl font-mono">Vasishta Malisetty</h1>
+      <p className="container mx-auto flex justify-center pt-2 text-2xl font-mono">Electrical & Computer Engineering @ Northeastern</p>
+      <img className="rounded-full w-64 h-64 container mx-auto p-6" src="/assets/images/Website-Photo.png"></img>
+      <a href="/assets/files/Vasishta-Malisetty-Resume-2025.pdf" target="_blank" rel="noopener noreferrer" className="flex w-full max-w-24 container mx-auto outline-solid bg-black-500 hover:bg-white text-white hover:text-black font-bold p-2 rounded font-mono justify-center">
         Resume
       </a>
     </div>
     <div className="p-10 container mx-auto max-w-4xl">
-      <h2 className="text-3xl">ABOUT</h2>
-      <p>Hey! My name is Vasishta Malisetty and I am an Electrical and Computer Engineering major at Northeastern University. Currently, I am a SysDev Co-op at Amazon! </p>
+      <h2 className="text-3xl underline underline-offset-4 py-2 font-mono">ABOUT</h2>
+      <p className="text-lg font-mono">Hey! My name is Vasishta Malisetty and I am an Electrical and Computer Engineering major at Northeastern University. Currently, I am a SysDev Co-op at Amazon! </p>
     </div>
     
     <div className="p-10 container mx-auto max-w-4xl">
-      <h2 className="text-3xl">BIO</h2>
+      <h2 className="text-3xl underline underline-offset-4 py-2 font-mono">BIO</h2>
       <h3>Fall 2004: Born in Pittsburgh, PA</h3>
       <h3>Summer 2018: Moved to Hopkinton, MA</h3>
       <h3>Summer 2022: Graduated from Hopkinton High School</h3>
@@ -29,13 +29,38 @@ export default function Home() {
     </div>
     
     <div className="p-10 container mx-auto max-w-4xl">
-      <h3 className="text-3xl">HOBBIES</h3>
-      <p>Pittsburgh Steelers, Chelsea FC, Chess, Basketball</p>
+      <h3 className="text-3xl underline underline-offset-4 py-2 font-mono">HOBBIES</h3>
+      <p className="text-lg font-mono">Pittsburgh Steelers, Chelsea FC, Chess, Basketball, Video Games, Board Games, Clash of Clans</p>
     </div>
 
     <div className="p-10 container mx-auto max-w-4xl">
-      <h3 className="text-3xl">LINKS</h3>
-      
+      <h3 className="text-3xl underline underline-offset-4 py-2 font-mono">LINKS</h3>
+      <a href="https://www.linkedin.com/in/vmalisetty/" target="_blank" rel="noopener noreferrer" className="inline-flex items-center justify-center p-3 text-base font-mono text-white rounded-lg hover:outline-solid">
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
+      <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
+      </svg>
+      <span className="w-full px-2">LinkedIn</span>
+      </a>
+      <a href="https://github.com/vmalisetty-23" target="_blank" rel="noopener noreferrer" className="inline-flex items-center justify-center p-3 text-base font-mono text-white rounded-lg hover:outline-solid">
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+      </svg>
+      <span className="w-full px-2">GitHub</span>
+      </a>
+      <a href="https://www.instagram.com/vmalisetty_23/" target="_blank" rel="noopener noreferrer" className="inline-flex items-center justify-center p-3 text-base font-mono text-white rounded-lg hover:outline-solid">
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+      </svg>
+      <span className="w-full px-2">Instagram</span>
+      </a>
+      <a href="https://outlook.office.com/mail/deeplink/compose/?mailtouri=mailto%3amalisetty.v%40northeastern.edu" target="_blank" rel="noopener noreferrer" className="inline-flex items-center justify-center p-3 text-base font-mono text-white rounded-lg hover:outline-solid">
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M20 10H4v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8ZM9 13v-1h6v1a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
+      <path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2Z"/>
+      </svg>
+      <span className="w-full px-2">Email</span>
+      </a>
     </div>
     </>
   );
