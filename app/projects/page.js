3d174acf5a2e4f7e697d21bc4ada87fd4f93b905
Vasishta Malisetty <malisetty.v@northeastern.edu>
2025-05-11 17:24:33 -0400

    Added Plaitpilot Project Card

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,75 @@
 export default function Projects() {
   return (
     <>
+    <div className="flex flex-wrap justify-center gap-6 pt-5">
+        <div className="max-w-sm bg-white rounded-xs">
+          <a>
+            <img
+              className="rounded-t-xs"
+              src="/assets/images/Plaitpilot.jpeg"
+              alt="Plaitpilot"
+            />
+          </a>
+          <div className="p-5">
+            <a>
+              <h5 className="mb-2 text-2xl font-mono tracking-tight text-black font-bold">
+                Plaitpilot
+              </h5>
+            </a>
+            <p className="mb-3 font-mono text-black">
+              Plaitpilot is a a machine that reduces hair extension prep time from hours to seconds.
+            </p>
+            <div className="flex gap-18">
+              <a
+                href="assets/images/Sensify_Blurb.png"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold inline-flex items-center px-5 py-2.5 text-sm font-mono text-center text-black rounded-lg dark:hover:outline-solid hover:outline-solid"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+              <a
+                href="https://github.com/smvumbaco/Sensify-FA24"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold inline-flex items-center px-5 py-2.5 text-sm font-mono text-center text-black rounded-lg hover:outline-solid"
+              >
+                GitHub
+                <svg
+                  className="w-6 h-6 ms-2 text-black"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-wrap justify-center gap-6 pt-5">
         <div className="max-w-sm bg-white rounded-xs">
           <a>
