cc08f32b3c29229e12e74a12ecb5dff26041ea8e
Vasishta Malisetty <malisetty.v@northeastern.edu>
2025-09-02 15:42:56 -0400

    Typed Title

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
           Vasishta Malisetty
         </h1>
         <p className="container mx-auto flex justify-center pt-2 pb-2 text-xl font-mono text-black dark:text-white">
-          ECE @ Northeastern
+          <span class ="typing">ECE @ Northeastern</span>
+          <span class="typed-cursor" aria-hidden="true">|</span>
         </p>
         <img
           className="rounded-full w-64 h-64 container mx-auto pt-2"
