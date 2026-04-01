a2c6f65c851c74d8fcdbaea8d16b1425735dcc1e
Vasishta Malisetty <malisetty.v@northeastern.edu>
2026-04-01 15:47:47 -0400

    Updated Homescreen pt.2

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-10 border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-black py-4 font-mono">
+    <nav className="sticky top-0 z-10 border-b border-gray-200 dark:border-gray-700 bg-background py-4 font-mono">
       <div className="container mx-auto flex items-center justify-between px-6">
         <Link href="/" className="text-lg font-bold text-black dark:text-white">
           Vasishta&apos;s Portfolio
         </Link>
         <ul className="flex text-lg">
           <li><Link href="/" className="text-black dark:text-white hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white p-4 rounded-xl">Home</Link></li>
+          <li><Link href="/#about" className="text-black dark:text-white hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white p-4 rounded-xl">About</Link></li>
+          <li><Link href="/#bio" className="text-black dark:text-white hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white p-4 rounded-xl">Bio</Link></li>
           <li><Link href="/projects" className="text-black dark:text-white hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white p-4 rounded-xl">Projects</Link></li>
           <li><Link href="/contact" className="text-black dark:text-white hover:text-white dark:hover:text-black hover:bg-black dark:hover:bg-white p-4 rounded-xl">Contact</Link></li>
         </ul>
